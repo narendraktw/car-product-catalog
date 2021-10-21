6f233bbdeadbb24738468b8c8122ab14fd44901b
narendraktw <narendraktw@gmail.com>
2021-10-21 08:37:06 +0530

    setup env file

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -17,7 +17,7 @@ const errorLink = onError(({ graphQLErrors, networkError }) => {
 
 const link = from([
   errorLink,
-  new HttpLink({ uri: "http://localhost:8000/graphql" })
+  new HttpLink({ uri: process.env.REACT_APP_API_DEV_URL })
 ])
 
 const client = new ApolloClient({
